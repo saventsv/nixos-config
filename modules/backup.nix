d393bb16115c7cafbb64f9da3cfaacb0e4522967
saventsv <saventv@gmail.com>
2026-07-03 11:18:15 -0500

    temporarily removed kde plasma due ot it pulling in a bunch of packages

--- modules/backup.nix
+++ modules/backup.nix
@@ -1,13 +1,5 @@
 {pkgs, config, ...}:
 {
   services.xserver.windowManager.i3.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    khelpcenter
-      konsole
-      plasma-browser-integration
-      kdeconnect-kde
-  ];
+  # services.desktopManager.plasma6.enable = true;
 }
--- modules/ui.nix
+++ modules/ui.nix
@@ -3,8 +3,6 @@
   environment.systemPackages = with pkgs; [
     polybar
     waybar
-    ags
-    quickshell
   ];
   
   fonts.packages = with pkgs; [
