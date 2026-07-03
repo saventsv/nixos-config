5dc3fb2985d3b40f25a3ddc99050ba7b03c20a11
saventsv <saventv@gmail.com>
2026-07-03 11:13:01 -0500

    fully removed home manager

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, ... }:
