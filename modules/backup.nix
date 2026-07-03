61af3cc8a104f43425a72bcffef2829fa8f23c7b
saventsv <saventv@gmail.com>
2026-07-03 11:16:22 -0500

    trying to remove packages from kde

--- modules/backup.nix
+++ modules/backup.nix
@@ -2,4 +2,12 @@
 {
   services.xserver.windowManager.i3.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    khelpcenter
+      konsole
+      plasma-browser-integration
+      kdeconnect-kde
+  ];
 }
