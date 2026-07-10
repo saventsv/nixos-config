07b2dc9d210d936bc38899b3f63d0fe3960f0c1a
saventsv <saventv@gmail.com>
2026-07-10 16:13:19 -0500

    changed the ssh keyring thing to use a real option this time

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -83,16 +83,17 @@
     promptInit = ''
       PS1="\u in \w \$ "
       '';
-      profileExtra = ''
-        if [ -z "$SSH_AUTH_SOCK" ]; then
-          eval "$(ssh-agent -s)"
-        fi
 
-        if ! ssh-add -l >/dev/null 2>&1; then
-          ssh-add ~/.ssh/id_ed25519
-        fi
-      '';
-  };
+    loginShellInit = ''
+      if [ -z "$SSH_AUTH_SOCK" ]; then
+        eval "$(ssh-agent -s)"
+      fi
+
+      if ! ssh-add -l >/dev/null 2>&1; then
+        ssh-add ~/.ssh/id_ed25519
+      fi
+    '';
+};
 
   environment.variables = { 
     EDITOR = "nvim";
