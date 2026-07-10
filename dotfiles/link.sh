411a5944af1053ca1a2b5db0e8dc22943df87a51
saventsv <saventv@gmail.com>
2026-07-10 13:12:17 -0500

    added a shell script for symlinking

---
+++ dotfiles/link.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/Pictures
+ln -snf ~/nixos-config/dotfiles/niri ~/.config/niri
+ln -snf ~/nixos-config/dotfiles/nvim ~/.config/nvim
+ln -snf ~/nixos-config/dotfiles/rofi ~/.config/rofi
+ln -snf ~/nixos-config/dotfiles/tmux ~/.config/tmux
+ln -snf ~/nixos-config/dotfiles/dunst ~/.config/dunst
+ln -snf ~/nixos-config/dotfiles/mango ~/.config/mango
+ln -snf ~/nixos-config/dotfiles/picom ~/.config/picom
+ln -snf ~/nixos-config/dotfiles/waybar ~/.config/waybar
+ln -snf ~/nixos-config/dotfiles/xmonad ~/.config/xmonad
+ln -snf ~/nixos-config/dotfiles/polybar ~/.config/polybar
+ln -snf ~/nixos-config/dotfiles/noctalia ~/.config/noctalia
+ln -snf ~/nixos-config/dotfiles/alacritty ~/.config/alacritty
+ln -snf ~/nixos-config/dotfiles/fastfetch ~/.config/fastfetch
+ln -snf ~/nixos-config/dotfiles/wallpapers/ ~/Pictures/wallpapers
