a0ea579b587c97165ec892574b6bc8fa732dccd2
saventsv <saventv@gmail.com>
2026-07-11 17:12:44 -0500

    changed the import in the laptop to point to the virt-manager module rather than the nonexistent virtualbox one

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,7 +8,7 @@
     # ../../modules/sessions/kde.nix
     ../../modules/sessions/xorg.nix
     ../../modules/programs/talos.nix
-    ../../modules/programs/virtualbox.nix
+    ../../modules/programs/virt-manager.nix
     ../../modules/programs/usbimager.nix
     ../../modules/sessions/dwm.nix
     ../../modules/sessions/kde.nix
