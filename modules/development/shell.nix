46f15868608d98d738894ab5be5a30fec9e674e7
saventsv <saventv@gmail.com>
2026-07-11 17:44:56 -0500

    changed the icon theme to papirus dark to see if it works better

--- modules/development/shell.nix
+++ modules/development/shell.nix
@@ -109,6 +109,6 @@
     MANPAGER="nvim +Man!";
     PATH="/home/saven/.local/bin/:$PATH";
     GTK_THEME = "Everforest-Dark";
-    GTK_ICON_THEME = "oomx-Everforest-Dark";
+    GTK_ICON_THEME = "Papirus-Dark";
   };
 }
