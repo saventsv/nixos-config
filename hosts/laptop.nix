d703abd6ab2185d51389e29a4bed7ec2bbd0cd4b
saventsv <saventv@gmail.com>
2026-07-31 15:09:34 -0500

    added mango back without changing the name to mango instead of mangowc

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -7,8 +7,7 @@
     ../modules/sessions/xorg.nix
     ../modules/sessions/wayland.nix
     ../modules/sessions/desktops/dwm.nix
-    # ../modules/sessions/desktops/mangowc.nix
-    # ../modules/sessions/desktops/kde.nix
+    ../modules/sessions/desktops/mangowc.nix
     ../modules/sessions/desktops/cosmic.nix
     ../modules/programs/talos.nix
     ../modules/services/battery.nix
