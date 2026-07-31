61a68cbf9fa81c1d9ac02b729574214ff5e2b5d1
saventsv <saventv@gmail.com>
2026-07-31 14:57:33 -0500

    removed mango because nix is being weird and telling me that mangowc was renamed to mango. When I fixed it said there was nothing named mango so IDK what is happening

--- modules/sessions/desktops/mangowc.nix
+++ modules/sessions/desktops/mangowc.nix
@@ -1,6 +1,7 @@
 { config, pkgs, inputs, ... }:
 {
   programs.mangowc.enable = true;
+
   environment.systemPackages = [
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
