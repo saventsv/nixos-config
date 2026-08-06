e9d311e6928baf5ed178c28e6ac1ddc25616a1f2
saventsv <saventv@gmail.com>
2026-08-06 10:52:20 -0500

    added the user to the network manager group for the desktop host

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -19,7 +19,7 @@
   system.stateVersion = "26.05"; 
   users.users.saven = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; 
+    extraGroups = [ "wheel" "networkmanager" ]; 
     packages = with pkgs; [
       tree
     ];
